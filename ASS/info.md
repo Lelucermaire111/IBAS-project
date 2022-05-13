2363f4139940c4ebefde5c1a0155cc56e1d12d78
Lelucermaire111 <1615738036@qq.com>
2022-05-14 01:47:03 +0800

    Update

---
+++ ASS/info.md
@@ -0,0 +1,5 @@
+## 信息记录
+### 百度云API
+1. AppID：26036539
+2. API KEY：qzcGvbNrcbbQxUbiB132Deea
+3. Secrecy key:l59UbAGAnER61mwyVIt44AgeUysnGXFe
--- MySQL/MySQL.py
+++ MySQL/MySQL.py
@@ -1,4 +1,5 @@
 import pymysql as mysql
+from Barcode import scancode
 mydb = mysql.connect(host = "sh-cynosdbmysql-grp-kxswlvau.sql.tencentcdb.com",
                      port = 20483,
                      user = "root",
@@ -25,4 +26,22 @@ def register(user_id, user_passwd):
     else:
         print("密码错误")
 
-register(1,'123456')
+# register(1,'123456')
+
+def searchISBN():
+    ISBN = scancode.ISBNScan()
+    mycursor.execute("SELECT name,author,memo FROM booklibrary WHERE isbn=%s LIMIT 1 OFFSET 0" %ISBN)
+    i = 0
+    for x in mycursor:
+        i = i + 1
+        name = x[0]
+        author = x[1]
+        memo = x[2]
+
+    if i == 0:
+        return -1
+    else:
+        return name,author,memo
+
+name,author,memo = searchISBN()
+print(name,author,memo)
